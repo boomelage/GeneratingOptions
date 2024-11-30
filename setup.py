28da7a0036fb6d8aaf85d37937bf389645b16a3c
Radu <163606270+boomelage@users.noreply.github.com>
2024-11-30 21:57:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author="RADU-STEFAN BRICIU",
     author_email="radusbriciu@proton.me",
     license="MIT",
-    description="a proprietary package of convenience wrappers for Python QuantLib",
     url="https://github.com/boomelage/GeneratingOptions",
     packages=find_packages(),
 )
